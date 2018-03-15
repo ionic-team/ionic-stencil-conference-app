5d621576dfddd02924eaf49d537d021c95813ba0
jgw96 <jgw9617@gmail.com>
2018-03-15 10:12:07 -0500

    chore(PWA): increase lighthouse score

--- stencil.config.js
+++ stencil.config.js
@@ -1,5 +1,14 @@
 exports.config = {
-  globalStyle: 'src/global.css'
+  globalStyle: 'src/global.css',
+  serviceWorker: {
+    globPatterns: [
+      '**/*.{js,css,json,html,ico,png,jpeg}'
+    ],
+    globIgnores: [
+      'build/app/svg/*.js',
+      'build/app/*.es5.js'
+    ]
+  }
 };
 
 exports.devServer = {
