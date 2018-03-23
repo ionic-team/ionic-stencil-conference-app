ae39a634f64e759f8fdc379a00f70bd519c5d63c
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2018-03-23 20:43:43 +0100

    chore(): update to latest

--- src/components.d.ts
+++ src/components.d.ts
@@ -3,6 +3,9 @@
  * It contains typing information for all components that exist in this project
  * and imports for stencil collections that might be configured in your stencil.config.js file
  */
+
+import '@stencil/core';
+
 declare global {
   namespace JSX {
     interface Element {}
@@ -24,467 +27,408 @@ import 'ionicons';
 import '@ionic/core';
 
 
-import {
-  AppRoot as AppRoot
-} from './components/app-root/app-root';
-
 declare global {
-  interface HTMLAppRootElement extends AppRoot, HTMLStencilElement {
+  interface HTMLAppRootElement extends HTMLStencilElement {
+
   }
   var HTMLAppRootElement: {
     prototype: HTMLAppRootElement;
     new (): HTMLAppRootElement;
   };
   interface HTMLElementTagNameMap {
-    "app-root": HTMLAppRootElement;
+    'app-root': HTMLAppRootElement;
   }
   interface ElementTagNameMap {
-    "app-root": HTMLAppRootElement;
+    'app-root': HTMLAppRootElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "app-root": JSXElements.AppRootAttributes;
+      'app-root': JSXElements.AppRootAttributes;
     }
   }
   namespace JSXElements {
     export interface AppRootAttributes extends HTMLAttributes {
-      
-      
+
     }
   }
 }
 
 
-import {
-  PageAboutPopover as PageAboutPopover
-} from './components/page-about-popover/page-about-popover';
-
 declare global {
-  interface HTMLPageAboutPopoverElement extends PageAboutPopover, HTMLStencilElement {
+  interface HTMLPageAboutPopoverElement extends HTMLStencilElement {
+
   }
   var HTMLPageAboutPopoverElement: {
     prototype: HTMLPageAboutPopoverElement;
     new (): HTMLPageAboutPopoverElement;
   };
   interface HTMLElementTagNameMap {
-    "page-about-popover": HTMLPageAboutPopoverElement;
+    'page-about-popover': HTMLPageAboutPopoverElement;
   }
   interface ElementTagNameMap {
-    "page-about-popover": HTMLPageAboutPopoverElement;
+    'page-about-popover': HTMLPageAboutPopoverElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-about-popover": JSXElements.PageAboutPopoverAttributes;
+      'page-about-popover': JSXElements.PageAboutPopoverAttributes;
     }
   }
   namespace JSXElements {
     export interface PageAboutPopoverAttributes extends HTMLAttributes {
-      
-      
+
     }
   }
 }
 
 
-import {
-  PageAbout as PageAbout
-} from './components/page-about/page-about';
-
 declare global {
-  interface HTMLPageAboutElement extends PageAbout, HTMLStencilElement {
+  interface HTMLPageAboutElement extends HTMLStencilElement {
+
   }
   var HTMLPageAboutElement: {
     prototype: HTMLPageAboutElement;
     new (): HTMLPageAboutElement;
   };
   interface HTMLElementTagNameMap {
-    "page-about": HTMLPageAboutElement;
+    'page-about': HTMLPageAboutElement;
   }
   interface ElementTagNameMap {
-    "page-about": HTMLPageAboutElement;
+    'page-about': HTMLPageAboutElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-about": JSXElements.PageAboutAttributes;
+      'page-about': JSXElements.PageAboutAttributes;
     }
   }
   namespace JSXElements {
     export interface PageAboutAttributes extends HTMLAttributes {
-      
-      
+
     }
   }
 }
 
 
-import {
-  PageAccount as PageAccount
-} from './components/page-account/page-account';
-
 declare global {
-  interface HTMLPageAccountElement extends PageAccount, HTMLStencilElement {
+  interface HTMLPageAccountElement extends HTMLStencilElement {
+
   }
   var HTMLPageAccountElement: {
     prototype: HTMLPageAccountElement;
     new (): HTMLPageAccountElement;
   };
   interface HTMLElementTagNameMap {
-    "page-account": HTMLPageAccountElement;
+    'page-account': HTMLPageAccountElement;
   }
   interface ElementTagNameMap {
-    "page-account": HTMLPageAccountElement;
+    'page-account': HTMLPageAccountElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-account": JSXElements.PageAccountAttributes;
+      'page-account': JSXElements.PageAccountAttributes;
     }
   }
   namespace JSXElements {
     export interface PageAccountAttributes extends HTMLAttributes {
-      
-      onUserDidLogOut?: (event: CustomEvent) => void;
+      'onUserDidLogOut'?: (event: CustomEvent) => void;
     }
   }
 }
 
 
-import {
-  PageLogin as PageLogin
-} from './components/page-login/page-login';
-
 declare global {
-  interface HTMLPageLoginElement extends PageLogin, HTMLStencilElement {
+  interface HTMLPageLoginElement extends HTMLStencilElement {
+
   }
   var HTMLPageLoginElement: {
     prototype: HTMLPageLoginElement;
     new (): HTMLPageLoginElement;
   };
   interface HTMLElementTagNameMap {
-    "page-login": HTMLPageLoginElement;
+    'page-login': HTMLPageLoginElement;
   }
   interface ElementTagNameMap {
-    "page-login": HTMLPageLoginElement;
+    'page-login': HTMLPageLoginElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-login": JSXElements.PageLoginAttributes;
+      'page-login': JSXElements.PageLoginAttributes;
     }
   }
   namespace JSXElements {
     export interface PageLoginAttributes extends HTMLAttributes {
-      
-      onUserDidLogIn?: (event: CustomEvent) => void;
+      'onUserDidLogIn'?: (event: CustomEvent) => void;
     }
   }
 }
 
 
-import {
-  PageMap as PageMap
-} from './components/page-map/page-map';
-
 declare global {
-  interface HTMLPageMapElement extends PageMap, HTMLStencilElement {
+  interface HTMLPageMapElement extends HTMLStencilElement {
+
   }
   var HTMLPageMapElement: {
     prototype: HTMLPageMapElement;
     new (): HTMLPageMapElement;
   };
   interface HTMLElementTagNameMap {
-    "page-map": HTMLPageMapElement;
+    'page-map': HTMLPageMapElement;
   }
   interface ElementTagNameMap {
-    "page-map": HTMLPageMapElement;
+    'page-map': HTMLPageMapElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-map": JSXElements.PageMapAttributes;
+      'page-map': JSXElements.PageMapAttributes;
     }
   }
   namespace JSXElements {
     export interface PageMapAttributes extends HTMLAttributes {
-      
-      
+
     }
   }
 }
 
 
-import {
-  PageScheduleFilter as PageScheduleFilter
-} from './components/page-schedule-filter/page-schedule-filter';
-
 declare global {
-  interface HTMLPageScheduleFilterElement extends PageScheduleFilter, HTMLStencilElement {
+  interface HTMLPageScheduleFilterElement extends HTMLStencilElement {
+
   }
   var HTMLPageScheduleFilterElement: {
     prototype: HTMLPageScheduleFilterElement;
     new (): HTMLPageScheduleFilterElement;
   };
   interface HTMLElementTagNameMap {
-    "page-schedule-filter": HTMLPageScheduleFilterElement;
+    'page-schedule-filter': HTMLPageScheduleFilterElement;
   }
   interface ElementTagNameMap {
-    "page-schedule-filter": HTMLPageScheduleFilterElement;
+    'page-schedule-filter': HTMLPageScheduleFilterElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-schedule-filter": JSXElements.PageScheduleFilterAttributes;
+      'page-schedule-filter': JSXElements.PageScheduleFilterAttributes;
     }
   }
   namespace JSXElements {
     export interface PageScheduleFilterAttributes extends HTMLAttributes {
-      
-      
+
     }
   }
 }
 
 
-import {
-  PageSchedule as PageSchedule
-} from './components/page-schedule/page-schedule';
-
 declare global {
-  interface HTMLPageScheduleElement extends PageSchedule, HTMLStencilElement {
+  interface HTMLPageScheduleElement extends HTMLStencilElement {
+
   }
   var HTMLPageScheduleElement: {
     prototype: HTMLPageScheduleElement;
     new (): HTMLPageScheduleElement;
   };
   interface HTMLElementTagNameMap {
-    "page-schedule": HTMLPageScheduleElement;
+    'page-schedule': HTMLPageScheduleElement;
   }
   interface ElementTagNameMap {
-    "page-schedule": HTMLPageScheduleElement;
+    'page-schedule': HTMLPageScheduleElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-schedule": JSXElements.PageScheduleAttributes;
+      'page-schedule': JSXElements.PageScheduleAttributes;
     }
   }
   namespace JSXElements {
     export interface PageScheduleAttributes extends HTMLAttributes {
-      
-      
+
     }
   }
 }
 
 
-import {
-  PageSession as PageSession
-} from './components/page-session/page-session';
-
 declare global {
-  interface HTMLPageSessionElement extends PageSession, HTMLStencilElement {
+  interface HTMLPageSessionElement extends HTMLStencilElement {
+    'goback': string;
+    'sessionId': string;
   }
   var HTMLPageSessionElement: {
     prototype: HTMLPageSessionElement;
     new (): HTMLPageSessionElement;
   };
   interface HTMLElementTagNameMap {
-    "page-session": HTMLPageSessionElement;
+    'page-session': HTMLPageSessionElement;
   }
   interface ElementTagNameMap {
-    "page-session": HTMLPageSessionElement;
+    'page-session': HTMLPageSessionElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-session": JSXElements.PageSessionAttributes;
+      'page-session': JSXElements.PageSessionAttributes;
     }
   }
   namespace JSXElements {
     export interface PageSessionAttributes extends HTMLAttributes {
-      goback?: string;
-      sessionId?: string;
-      
+      'goback'?: string;
+      'sessionId'?: string;
     }
   }
 }
 
 
-import {
-  PageSignup as PageSignup
-} from './components/page-signup/page-signup';
-
 declare global {
-  interface HTMLPageSignupElement extends PageSignup, HTMLStencilElement {
+  interface HTMLPageSignupElement extends HTMLStencilElement {
+
   }
   var HTMLPageSignupElement: {
     prototype: HTMLPageSignupElement;
     new (): HTMLPageSignupElement;
   };
   interface HTMLElementTagNameMap {
-    "page-signup": HTMLPageSignupElement;
+    'page-signup': HTMLPageSignupElement;
   }
   interface ElementTagNameMap {
-    "page-signup": HTMLPageSignupElement;
+    'page-signup': HTMLPageSignupElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-signup": JSXElements.PageSignupAttributes;
+      'page-signup': JSXElements.PageSignupAttributes;
     }
   }
   namespace JSXElements {
     export interface PageSignupAttributes extends HTMLAttributes {
-      
-      
+
     }
   }
 }
 
 
-import {
-  PageSpeakerDetail as PageSpeakerDetail
-} from './components/page-speaker-detail/page-speaker-detail';
-
 declare global {
-  interface HTMLPageSpeakerDetailElement extends PageSpeakerDetail, HTMLStencilElement {
+  interface HTMLPageSpeakerDetailElement extends HTMLStencilElement {
+    'speakerId': string;
   }
   var HTMLPageSpeakerDetailElement: {
     prototype: HTMLPageSpeakerDetailElement;
     new (): HTMLPageSpeakerDetailElement;
   };
   interface HTMLElementTagNameMap {
-    "page-speaker-detail": HTMLPageSpeakerDetailElement;
+    'page-speaker-detail': HTMLPageSpeakerDetailElement;
   }
   interface ElementTagNameMap {
-    "page-speaker-detail": HTMLPageSpeakerDetailElement;
+    'page-speaker-detail': HTMLPageSpeakerDetailElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-speaker-detail": JSXElements.PageSpeakerDetailAttributes;
+      'page-speaker-detail': JSXElements.PageSpeakerDetailAttributes;
     }
   }
   namespace JSXElements {
     export interface PageSpeakerDetailAttributes extends HTMLAttributes {
-      speakerId?: string;
-      
+      'speakerId'?: string;
     }
   }
 }
 
 
-import {
-  PageSpeakerList as PageSpeakerList
-} from './components/page-speaker-list/page-speaker-list';
-
 declare global {
-  interface HTMLPageSpeakerListElement extends PageSpeakerList, HTMLStencilElement {
+  interface HTMLPageSpeakerListElement extends HTMLStencilElement {
+
   }
   var HTMLPageSpeakerListElement: {
     prototype: HTMLPageSpeakerListElement;
     new (): HTMLPageSpeakerListElement;
   };
   interface HTMLElementTagNameMap {
-    "page-speaker-list": HTMLPageSpeakerListElement;
+    'page-speaker-list': HTMLPageSpeakerListElement;
   }
   interface ElementTagNameMap {
-    "page-speaker-list": HTMLPageSpeakerListElement;
+    'page-speaker-list': HTMLPageSpeakerListElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-speaker-list": JSXElements.PageSpeakerListAttributes;
+      'page-speaker-list': JSXElements.PageSpeakerListAttributes;
     }
   }
   namespace JSXElements {
     export interface PageSpeakerListAttributes extends HTMLAttributes {
-      
-      
+
     }
   }
 }
 
 
-import {
-  PageSupport as PageSupport
-} from './components/page-support/page-support';
-
 declare global {
-  interface HTMLPageSupportElement extends PageSupport, HTMLStencilElement {
+  interface HTMLPageSupportElement extends HTMLStencilElement {
+
   }
   var HTMLPageSupportElement: {
     prototype: HTMLPageSupportElement;
     new (): HTMLPageSupportElement;
   };
   interface HTMLElementTagNameMap {
-    "page-support": HTMLPageSupportElement;
+    'page-support': HTMLPageSupportElement;
   }
   interface ElementTagNameMap {
-    "page-support": HTMLPageSupportElement;
+    'page-support': HTMLPageSupportElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-support": JSXElements.PageSupportAttributes;
+      'page-support': JSXElements.PageSupportAttributes;
     }
   }
   namespace JSXElements {
     export interface PageSupportAttributes extends HTMLAttributes {
-      
-      
+
     }
   }
 }
 
 
-import {
-  PageTabs as PageTabs
-} from './components/page-tabs/page-tabs';
-
 declare global {
-  interface HTMLPageTabsElement extends PageTabs, HTMLStencilElement {
+  interface HTMLPageTabsElement extends HTMLStencilElement {
+
   }
   var HTMLPageTabsElement: {
     prototype: HTMLPageTabsElement;
     new (): HTMLPageTabsElement;
   };
   interface HTMLElementTagNameMap {
-    "page-tabs": HTMLPageTabsElement;
+    'page-tabs': HTMLPageTabsElement;
   }
   interface ElementTagNameMap {
-    "page-tabs": HTMLPageTabsElement;
+    'page-tabs': HTMLPageTabsElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-tabs": JSXElements.PageTabsAttributes;
+      'page-tabs': JSXElements.PageTabsAttributes;
     }
   }
   namespace JSXElements {
     export interface PageTabsAttributes extends HTMLAttributes {
-      
-      
+
     }
   }
 }
 
 
-import {
-  PageTutorial as PageTutorial
-} from './components/page-tutorial/page-tutorial';
-
 declare global {
-  interface HTMLPageTutorialElement extends PageTutorial, HTMLStencilElement {
+  interface HTMLPageTutorialElement extends HTMLStencilElement {
+
   }
   var HTMLPageTutorialElement: {
     prototype: HTMLPageTutorialElement;
     new (): HTMLPageTutorialElement;
   };
   interface HTMLElementTagNameMap {
-    "page-tutorial": HTMLPageTutorialElement;
+    'page-tutorial': HTMLPageTutorialElement;
   }
   interface ElementTagNameMap {
-    "page-tutorial": HTMLPageTutorialElement;
+    'page-tutorial': HTMLPageTutorialElement;
   }
   namespace JSX {
     interface IntrinsicElements {
-      "page-tutorial": JSXElements.PageTutorialAttributes;
+      'page-tutorial': JSXElements.PageTutorialAttributes;
     }
   }
   namespace JSXElements {
     export interface PageTutorialAttributes extends HTMLAttributes {
-      
-      
+
     }
   }
 }
